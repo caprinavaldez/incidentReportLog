68273f1c19e3ae965d2bca1f62481f910f10d826
April Liane <almelcho@uci.edu>
2018-03-20 21:06:50 -0700

    added incident controller

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
+const incidentsRoutes = require("./incidents");
 
 // Book routes
 router.use("/books", bookRoutes);
+router.use("/incidents", incidentsRoutes);
 
 module.exports = router;
