5723b6d6bb02a70b27ba599ed9a8f8f6b98f1fdc
CatalinaLai <catalina.lai87@gmail.com>
2018-03-21 22:14:52 -0700

    Selection option added to SignIn Form

--- client/src/pages/SignIn/SignIn.js
+++ client/src/pages/SignIn/SignIn.js
@@ -5,7 +5,7 @@ import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
 // import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/Form";
+import { Input, TextArea, FormBtn, Select } from "../../components/Form";
 
 class SignIn extends Component {
   state = {
@@ -61,81 +61,33 @@ class SignIn extends Component {
             <Jumbotron>
               <h1>Login / Signup</h1>
             </Jumbotron>
-            {/* <div className="login-wrap">
-    <div className="login-html">
-        <input id="tab-1" type="radio" name="tab" className="sign-in" checked>
-        <label for="tab-1" className="tab">Sign In</label>
-        <input id="tab-2" type="radio" name="tab" className="sign-up">
-        <label for="tab-2" className="tab">Sign Up</label>
-        <div className="login-form">
-            <div className="sign-in-htm">
-                <div className="group">
-                    <label for="user" className="label">Username</label>
-                    <input id="user" type="text" className="input">
-                </div>
-                <div className="group">
-                    <label for="pass" className="label">Password</label>
-                    <input id="pass" type="password" className="input" data-type="password">
-                </div>
-                <div className="group">
-                    <input id="check" type="checkbox" className="check" checked>
-                    <label for="check">
-                        <span className="icon"></span> Keep me Signed in</label>
-                </div>
-                <div className="group">
-                    <input type="submit" className="button" value="Sign In">
-                </div>
-                <div className="hr"></div>
-                <div className="foot-lnk">
-                    <a href="#forgot">Forgot Password?</a>
-                </div>
-            </div>
-            <div className="sign-up-htm">
-                <div className="group">
-                    <label for="user" className="label">Username</label>
-                    <input id="user" type="text" className="input">
-                </div>
-                <div className="group">
-                    <label for="pass" className="label">Password</label>
-                    <input id="pass" type="password" className="input" data-type="password">
-                </div>
-                <div className="group">
-                    <label for="pass" className="label">Repeat Password</label>
-                    <input id="pass" type="password" class="input" data-type="password">
-                </div>
-                <div className="group">
-                    <label for="pass" className="label">Email Address</label>
-                    <input id="pass" type="text" className="input">
-                </div>
-                <div className="group">
-                    <input type="submit" className="button" value="Sign Up">
-                </div>
-                <div className="hr"></div>
-                <div className="foot-lnk">
-                    <label for="tab-1">Already Member?</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>             */}
+            {}
             <form>
               <Input
-                value={this.state.title}
-                onChange={this.handleInputChange}
+                // value={this.state.title}
+                // onChange={this.handleInputChange}
+                hasLabel="true"
+                required="true"
                 name="email"
                 placeholder="Email (required)"
+                type="text"
               />
               <Input
-                value={this.state.author}
-                onChange={this.handleInputChange}
-                name="author"
-                placeholder="Author (required)"
+                // value={this.state.author}
+                // onChange={this.handleInputChange}
+                hasLabel="true"
+                required="true"
+                name="password"
+                placeholder="Password (required)"
               />
-              <TextArea
-                value={this.state.synopsis}
-                onChange={this.handleInputChange}
-                name="synopsis"
-                placeholder="Synopsis (Optional)"
+              <Select
+                // value={this.state.synopsis}
+                // onChange={this.handleInputChange}
+                hasLabel="true"
+                required="true"
+                name="business"
+                placeholder="Business Type (required)"
+                options="Chrome, Firefox, Edge, Internet Explorer"
               />
               <FormBtn
                 disabled={!(this.state.author && this.state.title)}
