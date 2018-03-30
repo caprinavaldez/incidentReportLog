370afb79e7402732aed232b40552eea34dce242d
April Liane <almelcho@uci.edu>
2018-03-29 21:49:00 -0700

    group by category method fixed

--- routes/api/incidents.js
+++ routes/api/incidents.js
@@ -2,15 +2,18 @@ const router = require("express").Router();
 const incidentController = require("../../controllers/incidentsController");
 
 // Matches with "/api/incidents"
-router.route("/")
+router
+  .route("/")
   .get(incidentController.findAll)
   .post(incidentController.create);
-  
+
+router
+  .route("/counts-by-category")
+  .get(incidentController.groupByCategory);
 
 // Matches with "/api/incidents/:id"
 router
   .route("/:id")
-  .get(incidentController.groupByUser)
   .get(incidentController.findById)
   .put(incidentController.update)
   .delete(incidentController.remove);
