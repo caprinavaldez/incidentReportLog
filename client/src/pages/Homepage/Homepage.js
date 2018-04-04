b6dd58661f724b52e13b6d1705890fd3f9d5cefe
CatalinaLai <catalina.lai87@gmail.com>
2018-04-03 19:02:53 -0700

    image carousel added to homepage

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -4,17 +4,19 @@ import { Col, Row, Container } from "../../components/Grid";
 import Footer from "../../components/Footer";
 import API from "../../utils/API";
 import './Homepage.css';
+import Carousel from 'nuka-carousel';
 
 class Homepage extends Component {
   state = {
     appName: "Accident to Action",
-    description: "We do things simple. Accident to Action has been in the business to assist industries by providing clean statistical data to insurance companies and businesses. We save time when reporting accidents, viewing the data, giving ideas and suggestions, and allowing for thousands to millions of dollars to be saved by companies.",
-    exampleVisuals: "Example Accident Images and Graphs will go here",
-    benefits: "Benefits of Usage description here",
+    description: "We do things simple. Accident to Action has been in the business to assist industries by providing clean statistical data to insurance companies and businesses. We save time when reporting accidents, viewing the data, giving ideas and suggestions, and allowing for thousands to millions of dollars to be saved by companies.",    benefits: "Benefits of Usage description here",
     benefitsColOne: "Lore ipsum dolor sit amet, ne wisi partem mentitum qui. Sea te nisl affert repudiare, nibh iudico sea ad. Augue molestiae te pri, an mea probo scripta. An vim nusquam epicurei liberavisse. Quot primis accusam eos ne, rebum scaevola complectitur has no. Sed ei tacimates iudicabit erroribus, sit aeterno accumsan tractatos in, essent philosophia mei in.",
     benefitsColTwo: "Id mel postea integre deleniti, omnis commune apeirian ut sea. Laudem mediocritatem ex pro, cum cu altera oporteat singulis, sea commodo constituto delicatissimi ei. Cu qui laudem delectus. Tantas recusabo per ea, prima omnium scribentur eum no. Eu vis habeo harum labores, sumo ferri sonet nec at.",
     benefitsColThree: "Eam iusto accusamus ei, et ius cibo graeci verear, ut mei pertinax sensibus ullamcorper. Mei ei equidem epicurei, duo ceteros pertinacia delicatissimi eu, iriure suscipit oportere eum in. Cu per agam habeo blandit, usu eligendi dignissim ex. Tation maluisset dignissim eam cu, alia scaevola ne pri, ei meis noster usu."
+
+
   };
+  
   // When this component mounts, grab the book with the _id of this.props.match.params.id
   // e.g. localhost:3000/books/599dcb67f0f16317844583fc
   componentDidMount() {
@@ -44,7 +46,23 @@ class Homepage extends Component {
           </Col>
         </Row>
         <Row>
-          <h2>{this.state.exampleVisuals}</h2>
+          <Col size="md-10 md-offset-1">
+          <div className="border">
+            <img src="/cautionTape.png" />
+          </div>
+          <div className="slideshowContainer">
+            <Carousel>
+              <img alt="Annual Average Statistics on Work Injurk Accidents" src="/slide1.png" />
+              <img alt="Slips, Trips, and Falls Statistics" src="/slide2.png" />
+              <img alt="Overexertion Statistics" src="/slide3.png" />
+              <img alt="Struck By... Statistics" src="/slide4.png" />
+              <img alt="Dangerous Industries" src="/slide5.png" />
+            </Carousel>
+          </div>
+            <div className="border">
+              <img src="/cautionTape.png" />
+            </div>
+          </Col>
         </Row>
         <Row>
           <h2>{this.state.benefits}</h2>
